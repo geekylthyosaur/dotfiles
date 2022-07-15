5387944c98b10d51f234576ccfba7e05d8e7f5f0
geekylthyosaur <geekylthyosaur@gmail.com>
2022-07-15 13:47:50 +0300

    Clear

--- qtile/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-picom -b &
-xset b off &
-xset r rate 235 35 &
-xinput set-prop "TPPS/2 Elan TrackPoint" "libinput Left Handed Enabled" 1 &
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1 &
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1 &
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Accel Speed" 0.4
