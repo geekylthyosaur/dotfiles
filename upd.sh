fd0bf3b9d8b7a998589cd4fe45b2fee4e5216e9d
geekylthyosaur <geekylthyosaur@gmail.com>
2022-07-20 11:50:44 +0300

    Upd.

---
+++ upd.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+cp -r $XDG_CONFIG_HOME/alacritty ./xdg_config_home/
+cp -r $XDG_CONFIG_HOME/fish ./xdg_config_home/
+cp -r $XDG_CONFIG_HOME/nvim ./xdg_config_home/
+cp -r $XDG_CONFIG_HOME/sway ./xdg_config_home/
+cp -r $XDG_CONFIG_HOME/waybar ./xdg_config_home/
+
+cp -r $XDG_DATA_HOME/style.css ./xdg_data_home/
+
+cp -r /etc/greetd .
--- xdg_config_home/fish/config.fish
+++ xdg_config_home/fish/config.fish
@@ -1,5 +1,3 @@
 if status is-interactive
 	set fish_greeting
-    set fish_cursor_normal 'block'
-    set fish_cursor_insert 'line'
 end
