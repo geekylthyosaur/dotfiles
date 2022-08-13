4b39967afa16d6e06e90c5049cfabd5ba4580926
geekylthyosaur <geekylthyosaur@gmail.com>
2022-08-13 22:07:46 +0300

    Upd.

--- scripts/power_menu.sh
+++ scripts/power_menu.sh
@@ -1,34 +1,17 @@
 #!/bin/sh
 
-i=$(printf "Suspend\nLog Out\nReload\nReboot\nShutdown" | 
-    fuzzel --font="Noto Sans Mono: size=9" \
-    --no-icons \
-    --background-color=282828f0 \
-    --text-color=ebdbb2ff \
-    --selection-color=ebdbb2ff \
-    --selection-text-color=282828ff \
-    --border-color=458588ff \
-    --border-radius=0 \
-    --horizontal-pad=10 \
-    --vertical-pad=10 \
-    --inner-pad=10 \
-    --lines=5 \
-    --dmenu \
-    --index
-    ) 
-if [ "$i" -eq "0" ];
-then 
-    exec doas zzz
-elif [ "$i" -eq "1" ];
-then 
-    exec swaymsg exit
-elif [ "$i" -eq "2" ];
-then 
-    exec swaymsg reload
-elif [ "$i" -eq "3" ];
-then 
-    exec doas reboot
-elif [ "$i" -eq "4" ];
-then 
-    exec doas shutdown -h now
-fi
+case "$(printf "Suspend\nLogout\nReboot\nShutdown" | 
+    dmenu \
+    -l 5 \
+    -fn "Noto Sans Mono: size=10" \
+    -nb "#282828" \
+    -nf "#ebdbb2" \
+    -sb "#ebdbb2" \
+    -sf "#282828" \
+    )" in
+        Suspend) exec systemctl suspend;;
+        Logout) i3-msg exit;;
+        Reboot) exec systemctl reboot;;
+        Shutdown) exec systemctl shutdown now;;
+esac
+
--- upd.sh
+++ upd.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
 cp -r $XDG_CONFIG_HOME/alacritty ./xdg_config_home/
-cp -r $XDG_CONFIG_HOME/fish ./xdg_config_home/
+# cp -r $XDG_CONFIG_HOME/fish ./xdg_config_home/
 cp -r $XDG_CONFIG_HOME/nvim/init.vim ./xdg_config_home/nvim/
-cp -r $XDG_CONFIG_HOME/sway ./xdg_config_home/
+# cp -r $XDG_CONFIG_HOME/sway ./xdg_config_home/
 cp -r $XDG_CONFIG_HOME/tmux ./xdg_config_home/
-cp -r $XDG_CONFIG_HOME/user-dirs.dirs ./xdg_config_home
-cp -r $XDG_CONFIG_HOME/waybar ./xdg_config_home/
+# cp -r $XDG_CONFIG_HOME/waybar ./xdg_config_home/
 cp -r $XDG_CONFIG_HOME/htop ./xdg_config_home/
 cp -r $XDG_CONFIG_HOME/i3 ./xdg_config_home/
 cp $XDG_CONFIG_HOME/user-dirs.dirs ./xdg_config_home/
 cp $XDG_CONFIG_HOME/mimeapps.list ./xdg_config_home/
 
-cp -r $XDG_DATA_HOME/style.css ./xdg_data_home/
+# cp -r $XDG_DATA_HOME/style.css ./xdg_data_home/
 
-cp -r /etc/greetd .
+# cp -r /etc/greetd .
 
 cp -r $HOME/.mozilla/firefox/*/chrome/ ./firefox/
 cp -r $HOME/documents/scripts ./
