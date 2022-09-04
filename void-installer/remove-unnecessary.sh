302e1979681e0b67a509403a7c9575e4c9eaa46b
geekylthyosaur <geekylthyosaur@gmail.com>
2022-09-04 18:29:03 +0300

    Upd.

--- void-installer/remove-unnecessary.sh
+++ void-installer/remove-unnecessary.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 echo "ignorepkg=sudo" | doas tee /etc/xbps.d/10-ignore.conf
 echo "ignorepkg=linux-firmware-broadcom" | doas tee -a /etc/xbps.d/10-ignore.conf
 echo "ignorepkg=linux-firmware-nvidia" | doas tee -a /etc/xbps.d/10-ignore.conf
