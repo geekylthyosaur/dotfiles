750825a6e88d010108c22d0c3ba26182a7bf2d2a
geekylthyosaur <geekylthyosaur@gmail.com>
2022-09-07 23:01:07 +0300

    Do not ask the user

--- void-installer/remove-unnecessary.sh
+++ void-installer/remove-unnecessary.sh
@@ -3,5 +3,5 @@
 echo "ignorepkg=sudo" | doas tee /etc/xbps.d/10-ignore.conf
 echo "ignorepkg=linux-firmware-broadcom" | doas tee -a /etc/xbps.d/10-ignore.conf
 echo "ignorepkg=linux-firmware-nvidia" | doas tee -a /etc/xbps.d/10-ignore.conf
-doas xbps-remove -Rf sudo linux-firmware-broadcom linux-firmware-nvidia
+doas xbps-remove -Rfy sudo linux-firmware-broadcom linux-firmware-nvidia
 
