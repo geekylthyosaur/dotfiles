7087edc072e7c4f3949c5dc36d500071469995d7
geekylthyosaur <geekylthyosaur@gmail.com>
2022-09-09 11:09:54 +0300

    Install missing screen share deps

--- void-installer/get-base.sh
+++ void-installer/get-base.sh
@@ -3,8 +3,9 @@
 doas xbps-install -y foot fuzzel neovim tmux htop \
     grim playerctl light \
     dbus polkit mesa-ati-dri \
-    pipewire alsa-pipewire \
+    pipewire alsa-pipewire libpipewire0.2 \
     xdg-desktop-portal xdg-desktop-portal-wlr v4l2loopback gstreamer1-pipewire slurp \
     noto-fonts-ttf noto-fonts-ttf-extra
 
 doas ln -s /etc/sv/dbus /var/service/
+doas ln -s /etc/sv/polkitd /var/service/
