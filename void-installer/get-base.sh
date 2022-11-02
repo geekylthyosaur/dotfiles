7387a1cfbe922ce6db80e9fc7b6198f1c937860c
geekylthyosaur <geekylthyosaur@gmail.com>
2022-11-02 23:00:17 +0200

    Upd.

--- void-installer/get-base.sh
+++ void-installer/get-base.sh
@@ -5,6 +5,7 @@ doas xbps-install -y foot fuzzel neovim tmux htop \
     dbus polkit mesa-ati-dri \
     pipewire alsa-pipewire libpipewire0.2 \
     xdg-desktop-portal xdg-desktop-portal-wlr v4l2loopback gstreamer1-pipewire slurp \
+    gsetting-desktop-schemas \
     noto-fonts-ttf noto-fonts-ttf-extra
 
 doas ln -s /etc/sv/dbus /var/service/
