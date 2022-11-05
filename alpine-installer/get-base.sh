ae22c38118734599b75cef94d5d35354dfa6bb32
geekylthyosaur <geekylthyosaur@gmail.com>
2022-11-05 23:42:33 +0200

    Alpine Linux installer: upd

--- alpine-installer/get-base.sh
+++ alpine-installer/get-base.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+while true; do
+    read -p "Install amd [1] or intel [2]? " option
+    case $option in
+        1 ) doas apk add mesa-ati-dri mesa-dri-gallium mesa-va-gallium; break;;
+        2 ) ; break;;
+    esac
+done
+
+
 doas apk add foot fuzzel neovim tmux htop \
     grim playerctl light \
-    dbus polkit mesa-ati-dri mesa-dri-gallium mesa-va-gallium \
+    dbus polkit \
     pipewire pipewire-alsa libpipewire0.2 \
     xdg-desktop-portal xdg-desktop-portal-wlr v4l2loopback gstreamer1-pipewire slurp \
     gsetting-desktop-schemas \
     ttf-dejavu
 
-doas ln -s /etc/sv/dbus /var/service/
-doas ln -s /etc/sv/polkitd /var/service/
