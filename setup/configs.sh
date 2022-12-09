68a48d51ce61e1c136535c2867a55da4aec3375e
geekylthyosaur <geekylthyosaur@gmail.com>
2022-12-09 22:18:12 +0200

    Fix after mv

--- setup/configs.sh
+++ setup/configs.sh
@@ -6,15 +6,15 @@ mkdir -p $XDG_CONFIG_HOME
 mkdir -p $XDG_DATA_HOME
 mkdir -p $XDG_CACHE_HOME
 
-cp -r ./xdg_config_home/user-dirs.dirs $XDG_CONFIG_HOME
+cp -r ../xdg_config_home/user-dirs.dirs $XDG_CONFIG_HOME
 # TODO: create dirs
-xdg-user-dirs-update
+# xdg-user-dirs-update
 
-cp -r ./xdg_config_home/alacritty $XDG_CONFIG_HOME
-cp -r ./xdg_config_home/htop $XDG_CONFIG_HOME
-cp -r ./xdg_config_home/tmux $XDG_CONFIG_HOME
-cp -r ./xdg_config_home/xdg-desktop-portal-wlr $XDG_CONFIG_HOME
-cp -r ./xdg_config_home/waybar $XDG_CONFIG_HOME
-cp -r ./xdg_config_home/sway $XDG_CONFIG_HOME
+cp -r ../xdg_config_home/alacritty $XDG_CONFIG_HOME
+cp -r ../xdg_config_home/htop $XDG_CONFIG_HOME
+cp -r ../xdg_config_home/tmux $XDG_CONFIG_HOME
+cp -r ../xdg_config_home/xdg-desktop-portal-wlr $XDG_CONFIG_HOME
+cp -r ../xdg_config_home/waybar $XDG_CONFIG_HOME
+cp -r ../xdg_config_home/sway $XDG_CONFIG_HOME
 
-cp ./xdg_data_home/style.css $XDG_DATA_HOME/
+cp ../xdg_data_home/style.css $XDG_DATA_HOME
