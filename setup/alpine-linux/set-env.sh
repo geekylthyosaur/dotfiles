e85a7f1fe5c8d1ab0b5e7f201081511abdbb2334
geekylthyosaur <geekylthyosaur@gmail.com>
2022-12-12 22:13:14 +0200

    Put all variables into single file

--- setup/alpine-linux/set-env.sh
+++ setup/alpine-linux/set-env.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "export XDG_CACHE_HOME=\$HOME/.cache" | doas tee /etc/profile.d/xdg.sh
-echo "export XDG_CONFIG_HOME=\$HOME/.config" | doas tee -a /etc/profile.d/xdg.sh
-echo "export XDG_DATA_HOME=\$HOME/.local/share" | doas tee -a /etc/profile.d/xdg.sh
-echo "export XDG_STATE_HOME=\$HOME/.local/state" | doas tee -a /etc/profile.d/xdg.sh
+echo "export XDG_CACHE_HOME=\$HOME/.cache" | doas tee /etc/profile.d/env.sh
+echo "export XDG_CONFIG_HOME=\$HOME/.config" | doas tee -a /etc/profile.d/env.sh
+echo "export XDG_DATA_HOME=\$HOME/.local/share" | doas tee -a /etc/profile.d/env.sh
+echo "export XDG_STATE_HOME=\$HOME/.local/state" | doas tee -a /etc/profile.d/env.sh
 
-echo "export CARGO_HOME=\$XDG_DATA_HOME/cargo" | doas tee /etc/profile.d/env.sh
+echo "export CARGO_HOME=\$XDG_DATA_HOME/cargo" | doas tee -a /etc/profile.d/env.sh
 echo "export RUSTUP_HOME=\$XDG_DATA_HOME/rustup" | doas tee -a /etc/profile.d/env.sh
 echo "export QT_QPA_PLATFORMTHEME=gtk3" | doas tee -a /etc/profile.d/env.sh
 
