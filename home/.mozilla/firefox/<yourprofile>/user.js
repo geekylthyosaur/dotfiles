89a44ff17e434ad14425d25b0d15ac0ab8ebd389
geekylthyosaur <geekylthyosaur@gmail.com>
2023-10-03 11:20:32 +0300

    Upd.

--- home/.mozilla/firefox/<yourprofile>/user.js
+++ home/.mozilla/firefox/<yourprofile>/user.js
@@ -1,3 +1,4 @@
 user_pref("browser.compactmode.show", true);
+user_pref("middlemouse.paste", false);
 user_pref("browser.tabs.tabmanager.enabled", false);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
