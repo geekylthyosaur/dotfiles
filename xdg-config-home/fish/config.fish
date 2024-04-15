ff30f6720e946a98944ba903b98e38401d718a6e
geekylthyosaur <geekylthyosaur@gmail.com>
2024-04-15 23:20:08 +0300

    Upd.

--- xdg-config-home/fish/config.fish
+++ xdg-config-home/fish/config.fish
@@ -11,6 +11,7 @@ set -x RUSTUP_HOME $XDG_DATA_HOME/rustup
 if status is-interactive
     set fish_greeting
 
+    fish_add_path ~/go/bin
     fish_add_path ~/.local/bin
     fish_add_path /sbin
     fish_add_path /usr/sbin
