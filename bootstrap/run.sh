e8da6f58d58d987572aaffa7d232ebca1b8047a1
geekylthyosaur <geekylthyosaur@gmail.com>
2024-11-13 13:20:33 +0200

    Bootstrap systemd units

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -euo pipefail
 
@@ -6,8 +6,14 @@ ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 HOSTS="$ROOTDIR/hosts"
 PLAYBOOK="$ROOTDIR/playbook.yaml"
 
-XDG_CONFIG_HOME="$HOME/.config"
+export XDG_BIN_HOME="$HOME/.local/bin"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+
+export XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR"
 
 sudo dnf install ansible -y
 
-ansible-playbook -i "$HOSTS" "$PLAYBOOK" --ask-become-pass -e "ROOTDIR=$ROOTDIR" -e "XDG_CONFIG_HOME=$XDG_CONFIG_HOME"
+ansible-playbook -i "$HOSTS" "$PLAYBOOK" --ask-become-pass -e "ROOTDIR=$ROOTDIR"
